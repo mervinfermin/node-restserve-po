a3cb4b71c6ffe492fdffbb1a57db1d24aa360d6e
Mervin <mervin_fermin@hotmail.com>
2022-04-29 09:58:50 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,26 @@
 // puertos
 //=========================================
 process.env.PORT = process.env.PORT || 3000;
+
+
+//entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// base de datos
+let urlDB;
+
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://127.0.0.1:27017/cafe';
+} else {
+
+    urlDB = 'mongodb://fama:cV476m4cfn6PvlTC@cluster0.670ig.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://fama:cV476m4cfn6PvlTC@cluster0.670ig.mongodb.net/cafe?retryWrites=true&w=majority';
+
+}
+process.env.URLDB = urlDB;
+
+//'mongodb+srv://fama:cV476m4cfn6PvlTC@cluster0.670ig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
